1de7f5c913dedbb2e4dd5b4ca18d430796a1c555
Harsha <harsha.rmb31@gmail.com>
2025-12-30 22:05:05 +0530

    updated cart.js

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -40,8 +40,6 @@ export function addtoCart(productId,selectorValue){
           }
            savetoStorage();
       };
-     
-
 export function removefromCart(productId){
  let updatedCart=[];
   cart.forEach((item)=>{
