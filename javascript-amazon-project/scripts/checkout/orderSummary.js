1dad7bd47b9c2c22b0b495573d1b8c7fa9848b4f
Harsha <harsha.rmb31@gmail.com>
2026-02-11 10:43:46 +0530

    used renderorderSmmary for delete removed DOM

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -131,8 +131,7 @@ export function renderOrderSummary(){
           removefromCart(productId);
           
 
-        const container= document.querySelector(`.js-cart-item-container-${productId}`);
-        container.remove();
+        renderOrderSummary();
         renderPaymentSummary();
         
         updateCartQuantity();
