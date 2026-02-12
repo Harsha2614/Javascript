048cb45d411472f3d1859cb3adbf5e1339eff624
Harsha <harsha.rmb31@gmail.com>
2026-02-12 23:01:32 +0530

    started working on CartTest

--- javascript-amazon-project/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project/tests-jasmine/data/cartTest.js
@@ -3,17 +3,7 @@ import {addtoCart, cart,loadFromStorage} from '../../data/cart.js';
 
 describe('test suite : addtoCart',()=>{
     it('adding an exiting product to the cart',()=>{
-        spyOn(localStorage,'setItem');
-        spyOn(localStorage,'getItem').and.callFake(()=>{
-            return JSON.stringify([{
-                id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                quantity:1,
-                
-                }
-            ]);
-
-        });
-        loadFromStorage();
+        
     
 
     });
