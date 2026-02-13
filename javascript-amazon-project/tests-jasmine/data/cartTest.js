fdd50d9fec21e28202a580f3bcd4ea8f45ba3d8c
Harsha <harsha.rmb31@gmail.com>
2026-02-13 21:11:36 +0530

    Started CartTest Existing product Test

--- javascript-amazon-project/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project/tests-jasmine/data/cartTest.js
@@ -3,6 +3,23 @@ import {addtoCart, cart,loadFromStorage} from '../../data/cart.js';
 
 describe('test suite : addtoCart',()=>{
     it('adding an exiting product to the cart',()=>{
+        spyOn(localStorage,'setItem');
+         spyOn(localStorage,'getItem').and.callFake(()=>{
+            return JSON.stringify([{
+                id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity:1,
+                deliveryOptionId:'1'
+
+            }]);
+
+        });
+        loadFromStorage();
+
+         addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+        expect(cart[0].id).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart[0].quantity).toEqual(2);
         
     
 
