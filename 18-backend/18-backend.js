043195eae4167b03d81602f137106d4c7d075bbb
Harsha <harsha.rmb31@gmail.com>
2026-03-04 16:24:01 +0530

    Practice Multiple backend requests

--- 18-backend/18-backend.js
+++ 18-backend/18-backend.js
@@ -1,24 +1,47 @@
-// const xhr=new XMLHttpRequest();
-// xhr.addEventListener('load',()=>{
-//     console.log(xhr.response);
-// })
-// xhr.open('GET','https://supersimplebackend.dev/greeting');
-// xhr.send();
+//callback
+const xhr=new XMLHttpRequest();
 
-// const promise=fetch('https://supersimplebackend.dev/greeting');
-// promise.then((response)=>{
-//    return response.text();
-// }).then((text)=>{
-//     console.log(text);
+xhr.addEventListener('load',()=>{
+    console.log(xhr.response);
+})
 
-// });  
+xhr.open('GET','https://supersimplebackend.dev/greeting');
+xhr.send();
 
+//promise
+const promise=fetch('https://supersimplebackend.dev/greeting');
+promise.then((response)=>{
+    return response.text();
+
+}).then((text)=>{
+    console.log(text)
+})
+
+//Async await
 
 async function getGreeting(){
     const response=await fetch('https://supersimplebackend.dev/greeting');
-    const text= await response.text();
-    console.log('text');
-
+    const text = await response.text();
+    console.log(text);
 }
 
 getGreeting();
+
+async function sendName(){
+    const response=await fetch('https://supersimplebackend.dev/greeting',{
+        method : 'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            name:"Harsha"
+        })
+    
+       }
+    );
+
+    const order=await response.text();
+    console.log(order);
+}
+
+sendName();
