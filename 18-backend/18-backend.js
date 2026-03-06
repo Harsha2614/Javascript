8b6099e951d5bceadcd2a7d85d0eb9d3e80569c6
Harsha <harsha.rmb31@gmail.com>
2026-03-06 10:45:19 +0530

    Practice on error handling

--- 18-backend/18-backend.js
+++ 18-backend/18-backend.js
@@ -1,3 +1,4 @@
+/*
 //callback
 const xhr=new XMLHttpRequest();
 
@@ -45,3 +46,83 @@ async function sendName(){
 }
 
 sendName();
+
+const prom=fetch('https://supersimplebackend.dev/greeting');
+prom.then((response)=>{
+    return response.text();
+}).then((response)=>{
+    console.log(response)
+})
+
+async function getGreet(){
+    const promise=await fetch('https://supersimplebackend.dev/greeting',{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            name :"Harsha"
+        })
+    })
+    const ans=await promise.text();
+    console.log(ans);
+}
+getGreet();
+*/
+
+/*
+//shows error because of cors
+
+const amazon=fetch("https://amazon.com");
+amazon.catch((error)=>{
+    console.log("Unable to fetch data")
+
+})
+
+async function amaze(){
+    try{
+
+        const promise=await fetch('https://amazon.com');
+    }
+    catch{
+        console.log("error at the backend")
+    }
+
+}
+
+amaze()
+*/
+
+async function practice(){
+
+   try{ 
+    const response=await fetch('https://supersimplebackend.dev/greeting',{
+        method:'POST',
+        headers:{
+            'Content-type':'application/json'
+        }
+    });
+
+        if(response.status>=400){
+            throw response;
+        }
+
+        const text = await response.text();
+        console.log(text);
+    }
+    catch(error){
+        if(error.status===404){
+            const errorMessage = await error.json();
+            console.log(errorMessage);
+        }
+        else {
+            console.log('Network error. Please try again later.');
+          }
+
+    }
+
+
+
+}
+
+practice()
